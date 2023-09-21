4cf46e55cb1dd535c9fd5a2ad9a3d7ad317729b7
smzuluaga01home <santimzuluaga01@hotmail.com>
2023-09-20 20:57:17 -0500

    avances en carrito de compras

--- js/script.js
+++ js/script.js
@@ -1,206 +1,25 @@
-// HEADER / BANNER -> SARA // HASTA LA LINEA 200
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// HEADER / BANNER -SANTI // LINEA 201 - 400 
-
-
+const cantidadProductos = 0;
+const listaProductos = [];
+
+const producto1 = document.getElementById("producto1-cat1")
+producto1.innerHTML = "Libreta 1"
+const producto2 = document.getElementById("producto2-cat1")
+producto2.innerHTML = "Libreta 2"
+const producto3 = document.getElementById("producto3-cat1")
+producto3.innerHTML = "Libreta 3"
+const producto4 = document.getElementById("producto4-cat1")
+producto4.innerHTML = "Libreta 4"
+const producto5 = document.getElementById("producto5-cat1")
+producto5.innerHTML = "Libreta 5"
+const producto6 = document.getElementById("producto6-cat1")
+producto6.innerHTML = "Libreta 6"
+const producto7 = document.getElementById("producto7-cat1")
+producto7.innerHTML = "Libreta 7"
+const producto8 = document.getElementById("producto8-cat1")
+producto8.innerHTML = "Libreta 8"
+const producto9 = document.getElementById("producto9-cat1")
+producto9.innerHTML = "Libreta 9"
 
 
 
